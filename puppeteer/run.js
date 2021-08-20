a1531e01ade29ca25eba80babab4819ee9f32beb
Michael Xueyuan Han-Vanbastelaer <hanx@g.harvard.edu>
2021-08-20 18:17:15 -0400

    Add code to get some stats from puppeteer.

--- puppeteer/run.js
+++ puppeteer/run.js
@@ -14,6 +14,9 @@ const puppeteer = require("puppeteer");
     const page = await browser.newPage();
     // Note: SSL not set up so do not use https!
     await page.goto("http://172.18.0.5");  // 172.18.0.5 is the flask application's IP
+    // Get some metrics. Ref: https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#pagemetrics
+    const metrics = await page.metrics();
+    console.log("Combined duration of all tasks performed by the browser: %d", metrics.TaskDuration);
     // await page.screenshot({path: "screenshot.png"});
 
     await page.close();
