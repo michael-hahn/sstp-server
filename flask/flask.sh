ca1e14b366c1b0a698ef8d8a2a46f5f1dab988de
Michael Xueyuan Han-Vanbastelaer <hanx@g.harvard.edu>
2021-08-19 22:53:03 -0400

    Add a reference for flask

--- flask/flask.sh
+++ flask/flask.sh
@@ -5,6 +5,7 @@
 
 cd ./sstp-server/flask
 # Build the docker image for flask server
+# Ref: https://www.digitalocean.com/community/tutorials/how-to-build-and-deploy-a-flask-application-using-docker-on-ubuntu-18-04
 docker build -t flask .
 
 # Ideally run the following in a separate console (e.g., using tmux)
