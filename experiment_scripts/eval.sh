7fab5f0b1407c221be7fe2038461c3f2b6d82de9
michael han <hanx@g.harvard.edu>
2021-09-11 20:31:43 -0400

    script that commands puppeteer experimnet

---
+++ experiment_scripts/eval.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Puppeteer experiment
+echo "1 2 3 4 5 6" | ./eval_puppeteer_workload.sh -w remote
+
+# echo "3" | ./eval_iperf_workload.sh -u true -t 30
+# echo "1 2 4 6 8 10" | ./eval_iperf_workload.sh -u false -t 60
+# echo "1" | ./eval_iperf_workload.sh -u false -t 60
+
